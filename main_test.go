cfd35458f2c7f8b349da06cd45c2ef9dd1fe1499
Ben Toews <mastahyeti@gmail.com>
2018-05-23 12:57:58 -0600

    infer digest/signature algos from public key type instead of cert sig

--- main_test.go
+++ main_test.go
@@ -1,12 +1,19 @@
 package cms
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+
 	"github.com/mastahyeti/fakeca"
 )
 
 var (
-	root         = fakeca.New(fakeca.IsCA)
-	intermediate = root.Issue(fakeca.IsCA)
-	leaf         = intermediate.Issue()
-	otherRoot    = fakeca.New(fakeca.IsCA)
+	root = fakeca.New(fakeca.IsCA)
+
+	intermediateKey, _ = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	intermediate       = root.Issue(fakeca.IsCA, fakeca.PrivateKey(intermediateKey))
+
+	leaf      = intermediate.Issue()
+	otherRoot = fakeca.New(fakeca.IsCA)
 )
